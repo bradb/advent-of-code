a15e92957508d53b73d60218a8e99bd1f318ac93
Brad Bollenbach <bradb@30sleeps.com>
2023-05-30 20:48:38 +0100

    day 2, part 2

--- 2021/elixir/day2.exs
+++ 2021/elixir/day2.exs
@@ -1,5 +1,5 @@
 defmodule AoC.Day2 do
-  def run(path) do
+  def part1(path) do
     input = File.stream!(path)
 
     %{d: d, h: h} =
@@ -24,6 +24,34 @@ defmodule AoC.Day2 do
 
     IO.puts(d * h)
   end
+
+  def part2(path) do
+    input = File.stream!(path)
+
+    %{d: d, h: h} =
+      input
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(fn s -> String.split(s, " ") end)
+      |> Enum.reduce(
+        %{d: 0, h: 0, a: 0},
+        fn [direction, step], acc = %{a: a} ->
+          case direction do
+            "forward" ->
+              Map.update!(acc, :h, fn v -> v + String.to_integer(step) end)
+              |> Map.update!(:d, fn v -> v + a * String.to_integer(step) end)
+
+            "down" ->
+              Map.update!(acc, :a, fn v -> v + String.to_integer(step) end)
+
+            "up" ->
+              Map.update!(acc, :a, fn v -> v - String.to_integer(step) end)
+          end
+        end
+      )
+
+    IO.puts(d * h)
+  end
 end
 
-AoC.Day2.run("./data/day2.txt")
+AoC.Day2.part1("./data/day2.txt")
+AoC.Day2.part2("./data/day2.txt")
